7dae6dca58c9ee46a905bd5a08471e578c4f1c46
Rachit Srivastava <honey032004@gmail.com>
2025-04-13 01:07:43 +0530

    feat: datatype and flowcontrol

---
+++ flowcontrol.go
@@ -0,0 +1,27 @@
+package main
+
+
+import(
+  "fmt"
+)
+
+func main(){
+// golang dosent have a while loop its just the same 
+//thing in for with conditions and incrementations
+  i := 1
+  for i <=3 {
+    fmt.Println(i)
+    i += 1;
+  }
+
+
+  for j := 0 ; j < 3 ; j++ {
+    fmt.Println(j)
+  }
+
+  for i := range 3{ //new way of setting range (0-(n-1))
+    fmt.Println(i)
+  }
+
+  //break and continue are same as cpp
+}
