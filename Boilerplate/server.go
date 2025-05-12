79fb80210c1d2b50a2a12c3554cdab2ea4204b75
Rachit Srivastava <honey032004@gmail.com>
2025-05-12 23:44:37 +0530

    feat: added boilerplate codes

---
+++ Boilerplate/server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+"fmt"
+"log"
+"net/http"
+"os"
+
+"github.com/go-chi/chi/v5"
+
+"github.com/joho/godotenv"
+
+"github.com/go-chi/cors"
+)
+
+func main() {
+
+  godotenv.Load()
+
+  portString := os.Getenv("PORT")
+  router := chi.NewRouter()
+
+  router.Use(cors.Handler(cors.Options{
+  // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+  AllowedOrigins: []string{"https://*", "http://*"},
+// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+  AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+  AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+  AllowCredentials: false,
+  MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
+  
+      v1Router := chi.NewRouter()
+
+      v1Router.Get("/healthz", HandlerReadiness)
+      v1Router.Get("/err", HandleErrors)
+
+      router.Mount("/v1", v1Router) // this nests another sub-router in the main router
+
+      srv := &http.Server{
+
+      Handler: router,
+      Addr:    ":" + portString,
+      }
+
+    fmt.Println("Port : ", portString)
+
+    log.Printf("Listening to port : %v", portString)
+
+    err := srv.ListenAndServe()
+    if err != nil {
+    log.Panic(err)
+    }
+
+}
