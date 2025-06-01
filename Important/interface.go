be5b710530d0983b4a2f81570a4be83d4f386701
Rachit Srivastava <honey032004@gmail.com>
2025-06-01 23:15:28 +0530

    Feat: more interface

--- Important/interface.go
+++ Important/interface.go
@@ -17,11 +17,23 @@ type circle struct {
 	radius float64
 }
 
+type square struct {
+	side float64
+}
+
 func (r rect) area() float64 {
 	return r.width * r.height
 }
 func (r rect) perim() float64 {
 	return 2*r.width + 2*r.height
+}
+
+func (r square) area() float64 {
+	return r.side * r.side
+}
+
+func (r square) perim() float64 {
+	return r.side * 4
 }
 
 func (c circle) area() float64 {
@@ -46,9 +58,11 @@ func detectCircle(g geometry) {
 func interfacePrac() {
 	r := rect{width: 3, height: 4}
 	c := circle{radius: 5}
+	s := square{side: 6}
 
 	measure(r)
 	measure(c)
+	measure(s)
 
 	detectCircle(r)
 	detectCircle(c)
