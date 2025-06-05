47950d51de0292968321d9ca085811ebebd0c6b4
Rachit Srivastava <honey032004@gmail.com>
2025-06-05 22:21:00 +0530

    enumsprac

---
+++ Important/enums.go
@@ -0,0 +1,17 @@
+package main
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
